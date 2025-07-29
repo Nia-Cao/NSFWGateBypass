8d6eb4fe36cfb008e3ff60ac023ff8a731c1beeb
Niamh <97837737+Nia-Cao@users.noreply.github.com>
2025-07-29 18:10:18 +0100

    Update NSFWGateBypass.user.js
    Modified for BetterDiscord, sets verification status to teen, restricting NSFW access and removing any bugging reminders to verify.

--- NSFWGateBypass.user.js
+++ NSFWGateBypass.user.js
@@ -1,25 +1,29 @@
-// ==UserScript==
-// @name         NSFWGateBypass
-// @namespace    https://skibidisalad.github.io/
-// @version      1.0
-// @description  Bypass Discord's age verification/NSFW gate
-// @author       SkibidiSalad
-// @match        https://discord.com/*
-// @grant        none
-// @run-at       document-start
-// ==/UserScript==
+/**
+     * @name NSFWGateBypassBD
+     * @author Kauht/Nia
+     * @description  Sets Discord age gate to teen, preventing NSFW access. Modified for BetterDiscord.
+     * @version 1.0.1
+     * @source https://github.com/Nia-Cao/NSFWGateBypass
+*/
+    module.exports = class SuckMyGate {
 
-(function() {
-  'use strict';
+        start() {
 
-  const interval = setInterval(() => {
-    try {
-      const modules = Object.values(webpackChunkdiscord_app.push([[Symbol()], {}, r => r.c]));
-      const userModule = modules.find(m => m?.exports?.default?.__proto__?.getCurrentUser);
-      if (userModule) {
-        userModule.exports.default.getCurrentUser().ageVerificationStatus = 3;
-        clearInterval(interval);
-      }
-    } catch (e) {}
+            'use strict';
+
+            const interval = setInterval(() => {
+              try {
+                const modules = Object.values(webpackChunkdiscord_app.push([[Symbol()], {}, r => r.c]));
+                const userModule = modules.find(m => m?.exports?.default?.__proto__?.getCurrentUser);
+                if (userModule) {
+                  userModule.exports.default.getCurrentUser().ageVerificationStatus = 2;
+                  clearInterval(interval);
+                }
+              } catch (e) {}
   }, 500);
-})();
+
+        }
+
+        stop() {
+        };
+}
